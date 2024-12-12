6784b936c24c9954fc0d1077299c451b4d3b53a1
Angelos Markos <amarkos@gmail.com>
2024-12-12 20:45:35 +0200

    Add files via upload

--- DIBmix.R
+++ DIBmix.R
@@ -71,107 +71,115 @@ DIBmix <- function(X, ncl, catcols, contcols, randinit = NULL,
   }
 
   cat_rel_imp <- 2
-  # Helper function to preprocess data
-  preprocess_data <- function(X, catcols, contcols) {
-    X <- data.frame(X)
-    for (i in catcols) {
-      X[, i] <- factor(X[, i], levels = unique(X[, i]), labels = seq_along(unique(X[, i])))
-    }
-    if (length(contcols) == 1) {
-      if (scale == TRUE)
-        X[, contcols] <- as.numeric(scale(X[, contcols]))
-    } else {
-      if (scale == TRUE)
-        X[, contcols] <- scale(X[, contcols])
-    }
-    return(X)
+  # Get ratio of categorical variables
+  cat_ratio <- length(catcols)/(ncol(X))
+  # Indicator for whether \zeta_j is determined by \ksi_j
+  cat_first <- FALSE
+
+  X <- data.frame(X)
+  for (i in catcols) {
+    X[, i] <- factor(X[, i], levels = unique(X[, i]), labels = seq_along(unique(X[, i])))
+  }
+  if (length(contcols) == 1) {
+    if (scale == TRUE)
+      X[, contcols] <- as.numeric(scale(X[, contcols]))
+  } else {
+    if (scale == TRUE)
+      X[, contcols] <- scale(X[, contcols])
   }
 
-  # Helper function to compute s using the bandwidth heuristic
-  compute_bandwidth <- function(X, contcols, s) {
-    if (s == -1) {
-      s_seq <- seq(0.1, 10, by = 1e-1)
-      for (s_val in s_seq) {
-        pxy_list_cont <- coord_to_pxy_R(as.data.frame(X[, contcols]), s = s_val,
-                                        cat_cols = c(), cont_cols = seq_along(contcols),
+  # Bandwidth values
+  if (length(s) == 1){
+    if (s == -1){
+      s_seq <- seq(0.1, 10, by=1e-1)
+      for (s_val in s_seq){
+        pxy_list_cont <- coord_to_pxy_R(as.data.frame(X[, contcols]), s = s_val, cat_cols = c(),
+                                        cont_cols = c(1:length(contcols)),
                                         lambda = 0)
         pyx_cont <- pxy_list_cont$py_x
-        avg_py_x <- mean(apply(pyx_cont, 2, function(x) max(x) / max(x[-which.max(x)])))
-        if (avg_py_x < 1.1) {
-          return(s_val - 1e-1)
+        avg_py_x <- mean(apply(pyx_cont, 2, FUN = function(x) max(x)/max(x[-which.max(x)])))
+        if (avg_py_x < (1.1)){
+          s <- s_val - 1e-1
+          avg_max_min_py_x <- mean(apply(pyx_cont, 2, FUN = function(x) max(x)/min(x)))
+          break
         }
       }
     }
-    return(s)
+  } else {
+    pxy_list_cont <- coord_to_pxy_R(as.data.frame(X[, contcols]), s = s, cat_cols = c(),
+                                    cont_cols = c(1:length(contcols)),
+                                    lambda = 0)
+    pyx_cont <- pxy_list_cont$py_x
   }
-
-  # Helper function to compute lambda
-  compute_lambda <- function(X, catcols, contcols, avg_max_min_py_x, cat_ratio, cat_rel_imp) {
+  if (length(lambda) == 1 && lambda == -1){
     num_lvls_vec <- sapply(X[, catcols, drop = FALSE], function(x) length(unique(x)))
-    cat_rel_imp <- min(2, avg_max_min_py_x^(1 / length(contcols)))
-    if (cat_rel_imp == 2 & cat_ratio > 0.5) {
+    cat_rel_imp <- min(2, avg_max_min_py_x^(1/length(contcols)))
+    # Check if cat_rel_imp is 2 & cat_ratio > 0.5
+    if (cat_rel_imp == 2 & cat_ratio > 0.5){
       cat_rel_imp <- 2 - cat_ratio
+      cat_first <- TRUE
     }
-    lambda <- (num_lvls_vec - 1) / (num_lvls_vec + cat_rel_imp - 1)
-    ordvars <- sapply(catcols, function(var) is.ordered(X[, var]))
-    if (any(ordvars)) {
-      inxs <- which(ordvars)
-      lambda[inxs] <- (1 / cat_rel_imp)^(1 / (num_lvls_vec[inxs] - 1))
+    ##lambda <- (num_lvls_vec-1)/(num_lvls_vec)
+    lambda <- (num_lvls_vec - 1)/(num_lvls_vec + cat_rel_imp - 1)
+    # Check for ordinal variables
+    ordvars <- c()
+    for (var in catcols){
+      if (is.ordered(X[, var])){
+        ordvars <- c(ordvars, var)
+      }
     }
-    return(lambda)
+    if (length(ordvars) > 0){
+      inxs <- match(ordvars, catcols)
+      lambda[inxs] <- (1/cat_rel_imp)^(1/(num_lvls_vec[inxs]-1))
+    }
   }
 
-  # Preprocessing
-  cat_ratio <- length(catcols) / ncol(X)
-  cat_first <- FALSE
-  X <- preprocess_data(X, catcols, contcols)
+  if (cat_first){
+    s_seq <- seq(0.1, 10, by=1e-1)
+    for (s_val in s_seq){
+      pxy_list_cont <- coord_to_pxy_R(as.data.frame(X[, contcols]), s = s_val, cat_cols = c(),
+                                      cont_cols = c(1:length(contcols)),
+                                      lambda = 0)
+      pyx_cont <- pxy_list_cont$py_x
+      avg_max_min_py_x <- mean(apply(pyx_cont, 2, FUN = function(x) max(x)/min(x)))
+      if (avg_max_min_py_x < (cat_rel_imp)^(length(catcols))){
+        s <- s_val
+        break
+      }
+    }
+  }
 
-  # Bandwidth computation
-  if (length(s) == 1)
-    if (s == -1)
-      s <- compute_bandwidth(X, contcols, s)
-
-
-  # Compute py_x for continuous variables
-  pxy_list_cont <- coord_to_pxy_R(as.data.frame(X[, contcols]), s = s, cat_cols = c(),
-                                  cont_cols = seq_along(contcols), lambda = 0)
-  pyx_cont <- pxy_list_cont$py_x
-  avg_max_min_py_x <- mean(apply(pyx_cont, 2, function(x) max(x) / min(x)))
-
-  if (length(lambda) == 1)
-    if (lambda == -1)
-      # Compute lambda
-      lambda <- compute_lambda(X, catcols, contcols, avg_max_min_py_x, cat_ratio, cat_rel_imp)
-
-  # Final bandwidth values
-  bw <- numeric(ncol(X))
+  bw <- rep(NA, ncol(X))
   bw[contcols] <- s
+  ##bw[catcols] <- lambda - eps_star
   bw[catcols] <- lambda
-
-  # Feature selection
-  if (select_features) {
-    bws_vec <- eigengap(data = X, contcols = contcols, catcols = catcols, bw = bw, ncl = ncl)
+  # Use eigengap heuristic for feature selection
+  if (select_features){
+    bws_vec <- eigengap(data = X, contcols = contcols,
+                        catcols = catcols,
+                        bw = bw, ncl = ncl)
   } else {
     bws_vec <- bw
   }
-  print(bws_vec[catcols])
   # Construct joint density with final bandwidths
-  pxy_list <- coord_to_pxy_R(X, s = bws_vec[contcols], cat_cols = catcols,
-                             cont_cols = contcols, lambda = bws_vec[catcols])
+  pxy_list <- coord_to_pxy_R(X, s = bws_vec[contcols],
+                             cat_cols = catcols, cont_cols = contcols,
+                             lambda = bws_vec[catcols])
+
   py_x <- pxy_list$py_x
   px <- pxy_list$px
+  pxy <- pxy_list$pxy
   hy <- pxy_list$hy
 
-  # Run DIBmix iteration
-  best_clust <- DIBmix_iterate(X, ncl = ncl, randinit = randinit, tol = 0,
-                               py_x = py_x, hy = hy, px = px, maxiter = maxiter,
-                               bws_vec = bws_vec, contcols = contcols,
-                               catcols = catcols, runs = nstart)
+  ######################################################
+  best_clust <- DIBmix_iterate(X, ncl = ncl, randinit = randinit,
+                               tol = 0, py_x, hy, px, maxiter,
+                               bws_vec, contcols, catcols,
+                               runs = nstart)
+  ######################################################
 
-  # Warning if clustering failed
-  if (best_clust$MutualInfo == Inf) {
+  if (best_clust$MutualInfo == Inf){
     warning("Initial cluster assignment remained unchanged; use other hyperparameter values for DIBmix to converge.")
   }
-
   return(best_clust)
 }
