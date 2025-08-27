2d65a209899e6310f8d49b91051d20ab6cf7570f
Costa <efthymios.costa@gmail.com>
2025-08-27 11:46:19 +0300

    update IBmix

--- R/AIBmix.R
+++ R/AIBmix.R
@@ -1,25 +1,25 @@
-AIBmix <- function(X, catcols, contcols, lambda = -1,
-                 s = -1, scale = TRUE,
-                 contkernel = "gaussian",
-                 nomkernel = "aitchisonaitken",
-                 ordkernel = "liracine",
-                 cat_first = FALSE) {
+AIBmix <- function(X, s = -1, lambda = -1,
+                   scale = TRUE,
+                   contkernel = "gaussian",
+                   nomkernel = "aitchisonaitken",
+                   ordkernel = "liracine",
+                   cat_first = FALSE) {
   
   # Validate inputs
   if (!is.data.frame(X)) {
     stop("Input 'X' must be a data frame.")
   }
-  if (!all(catcols %in% seq_along(X))) {
-    stop("Some 'catcols' indices are out of bounds or invalid.")
-  }
+  #if (!all(catcols %in% seq_along(X))) {
+  #  stop("Some 'catcols' indices are out of bounds or invalid.")
+  #}
   
-  if (!all(contcols %in% seq_along(X))) {
-    stop("Some 'contcols' indices are out of bounds or invalid.")
-  }
+  #if (!all(contcols %in% seq_along(X))) {
+  #  stop("Some 'contcols' indices are out of bounds or invalid.")
+  #}
   
-  if (any(duplicated(c(catcols, contcols)))) {
-    stop("'catcols' and 'contcols' must not overlap.")
-  }
+  #if (any(duplicated(c(catcols, contcols)))) {
+  #  stop("'catcols' and 'contcols' must not overlap.")
+  #}
   
   if (!is.logical(scale)) {
     stop("'scale' must be a logical value (TRUE or FALSE).")
@@ -38,54 +38,95 @@ AIBmix <- function(X, catcols, contcols, lambda = -1,
     stop("'ordkernel' can only be one of 'liracine' or 'wangvanryzin'")
   }
   
+  X <- data.frame(X)
+  # Check catcols/contcols
+  catcols <- as.integer(which(sapply(X, is.factor)))
+  if (length(catcols) == 0){
+    catcols <- c()
+  }
+  contcols <- as.integer(which(sapply(X, is.numeric)))
+  if (length(contcols) == 0){
+    contcols <- c()
+  }
+  
   if (cat_first & any(c(s, lambda) != -1)){
     stop("'cat_first' can only be TRUE when all bandwidths are determined by the algorithm (s = -1, lambda = -1).")
   }
-  
-  # Validate lambda
-  if (!is.numeric(lambda) ||
-      !(length(lambda) == 1 || length(lambda) == length(catcols)) ||
-      any(lambda <= 0 & lambda != -1)) {
-    stop("'lambda' must be either a single numeric value (-1 for automatic selection or a positive value) or a numeric vector with positive values matching the number of 'catcols'.")
+  if (cat_first & length(catcols) == 0){
+    stop("'cat_first' can only be TRUE when there are categorical variables in the data set.")
   }
   
-  # Additional check for maximum lambda value for nominal variables
-  if (length(lambda) > 1 && length(lambda) == length(catcols)) {
-    if (nomkernel == "liracine"){
-      max_lambda <- sapply(catcols, function(col) {
-        l <- length(unique(X[, col]))
-        (l - 1) / l
-      })
-    } else {
-      max_lambda <- 1
+  # Validate lambda if any categorical columns exist
+  if (length(catcols) > 0){
+    if (!is.numeric(lambda) ||
+        !(length(lambda) == 1 || length(lambda) == length(catcols)) ||
+        any(lambda <= 0 & lambda != -1)) {
+      stop("'lambda' must be either a single numeric value (-1 for automatic selection or a positive value) or a numeric vector with positive values matching the number of 'catcols'.")
     }
-    if (any(lambda > max_lambda)) {
-      stop("'lambda' values for nominal variables must not exceed their maximum allowable value.")
+    # Additional check for maximum lambda value for nominal variables
+    if (length(lambda) > 1 && length(lambda) == length(catcols)) {
+      if (nomkernel == "liracine"){
+        max_lambda <- sapply(catcols, function(col) {
+          l <- length(unique(X[, col]))
+          (l - 1) / l
+        })
+      } else {
+        max_lambda <- 1
+      }
+      if (any(lambda > max_lambda)) {
+        stop("'lambda' values for nominal variables must not exceed their maximum allowable value.")
+      }
     }
+    X[, catcols] <- preprocess_cat_data(X[, catcols])
   }
-  
   
   # Validate s
-  if (!is.numeric(s) ||
-      !(length(s) == 1 || length(s) == length(contcols)) ||
-      any(s <= 0 & s != -1)) {
-    stop("'s' must be either a single numeric value (-1 for automatic selection or a positive value) or a numeric vector with positive values matching the number of 'contcols'.")
+  if (length(contcols) > 0){
+    if (!is.numeric(s) ||
+        !(length(s) == 1 || length(s) == length(contcols)) ||
+        any(s <= 0 & s != -1)) {
+      stop("'s' must be either a single numeric value (-1 for automatic selection or a positive value) or a numeric vector with positive values matching the number of 'contcols'.")
+    }
+    if (scale){
+      X[, contcols] <- as.data.frame(preprocess_cont_data(X[, contcols]))
+    }
   }
   
-  X <- data.frame(X)
-  X[, catcols] <- preprocess_cat_data(X[, catcols])
-  if (scale){
-    X[, contcols] <- as.data.frame(preprocess_cont_data(X[, contcols]))
+  if (length(contcols) == 0){
+    if (length(lambda) == 1){
+      if (lambda == -1){
+        # Compute lambda for categorical data
+        lambda <- compute_lambda_cat(X, nomkernel, ordkernel)
+      }
+    }
+    bws_vec <- lambda
+  } else if (length(catcols) == 0){
+    if (length(s) == 1){
+      if (s == -1){
+        s <- compute_bandwidth_cont(X, contkernel = contkernel)
+      }
+    }
+    bws_vec <- rep(s, length(contcols))
+  } else {
+    bws_vec <- compute_s_lambda(X, contcols, catcols, s, lambda,
+                                contkernel, nomkernel, ordkernel,
+                                cat_first)
   }
-  
-  bws_vec <- compute_s_lambda(X, contcols, catcols, s, lambda,
-                              contkernel, nomkernel, ordkernel,
-                              cat_first)
   
   # Construct joint density with final bandwidths
-  pxy_list <- coord_to_pxy_R(X, s = bws_vec[contcols],
-                             cat_cols = catcols, cont_cols = contcols,
-                             lambda = bws_vec[catcols],
+  pxy_list <- coord_to_pxy_R(as.data.frame(X),
+                             s = if (length(contcols) > 0){
+                               bws_vec[contcols]
+                               } else {
+                                 -1
+                                 },
+                             lambda = if (length(catcols) > 0){
+                               bws_vec[catcols]
+                             } else {
+                               -1
+                             },
+                             cat_cols = catcols,
+                             cont_cols = contcols,
                              contkernel = contkernel,
                              nomkernel = nomkernel,
                              ordkernel = ordkernel)
@@ -94,9 +135,19 @@ AIBmix <- function(X, catcols, contcols, lambda = -1,
   # For AIB, we need strictly non-zero values in pxy...
   while (sum(pxy == 0) > 0){
     bws_vec[contcols] <- bws_vec[contcols] + 1e-1
-    pxy_list <- coord_to_pxy_R(X, s = bws_vec[contcols],
-                               cat_cols = catcols, cont_cols = contcols,
-                               lambda = bws_vec[catcols],
+    pxy_list <- coord_to_pxy_R(as.data.frame(X),
+                               s = if (length(contcols) > 0){
+                                 bws_vec[contcols]
+                               } else {
+                                 -1
+                               },
+                               lambda = if (length(catcols) > 0){
+                                 bws_vec[catcols]
+                               } else {
+                                 -1
+                               },
+                               cat_cols = catcols,
+                               cont_cols = contcols,
                                contkernel = contkernel,
                                nomkernel = nomkernel,
                                ordkernel = ordkernel)
@@ -111,5 +162,9 @@ AIBmix <- function(X, catcols, contcols, lambda = -1,
   attr(dendrogram, "call") <- NULL
   best_clust[[length(best_clust)+1]] <- dendrogram
   names(best_clust)[length(best_clust)] <- "dendrogram"
+  best_clust[[length(best_clust) + 1]] <- as.vector(s)
+  names(best_clust)[length(best_clust)] <- "s"
+  best_clust[[length(best_clust) + 1]] <- as.vector(lambda)
+  names(best_clust)[length(best_clust)] <- "lambda"
   return(best_clust)
 }
